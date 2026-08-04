36225b26fcc2fd03ec6a6dbb22c9ff6e69b87744
eliozashvili <ilivhsazoile.g@gmail.com>
2026-08-04 16:31:21 +0400

    feat: fill array with random numbers

--- Assets/Scripts/SlotsMachine.cs
+++ Assets/Scripts/SlotsMachine.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class SlotsMachine : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int[] _reelSlotsRandomNumbers = new int[3];
+
+    private void Start()
     {
-        
+        Spin();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spin()
     {
-        
+        for (var i = 0; i < _reelSlotsRandomNumbers.Length; i++)
+        {
+            _reelSlotsRandomNumbers[i] = Random.Range(0, 10);
+        }
+
+        Debug.Log(string.Join(", ", _reelSlotsRandomNumbers));
     }
 }
